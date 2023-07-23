ded66b396717a74c9686d011fcad1bb4f2284fb7
matheusnb99 <matheusintelectual123@hotmail.com>
2023-07-23 17:05:31 +0200

    add auth headers directly inside axios instance

--- src/lib/utils/instance.ts
+++ src/lib/utils/instance.ts
@@ -2,10 +2,21 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://localhost:3000/api/",
-  timeout: 5000,
+  timeout: 30000,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("session_jwt");
+    if (token) {
+      config.headers.Authorization = `${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default instance;
